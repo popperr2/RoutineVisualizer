5537913ca599b949fa57c817f1b6b1f2160fa8ae
Rebecca Popper <popperr2@tcnj.edu>
2017-11-27 15:58:22 -0500

    Image carousel created

--- code/Application/RoutineViewControllerAddNew.swift
+++ code/Application/RoutineViewControllerAddNew.swift
@@ -10,10 +10,19 @@
  */
 
 import UIKit
+import SQLite
 
-class RoutineViewControllerAddNew: UIViewController {
+class RoutineViewControllerAddNew: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
     // Class Variables
+    var imageArray = [UIImage(named: "stepImageTemp1"), UIImage(named: "stepImageTemp1"), UIImage(named: "stepImageTemp1"), UIImage(named: "stepImageTemp1"), UIImage(named: "stepImageTemp1"), UIImage(named: "stepImageTemp1"), UIImage(named: "stepImageTemp1"), ]
+    
+    var collectionView: UICollectionView?
+    let cellId = "imageCell"
+    let cellSpacing: CGFloat = 10
+    let cellWidth: Int = 150
+    let cellHeight: Int = 150
+    
     @IBOutlet weak var nameTextField: UITextField!  // The text field that takes in the User's Input for the Routine Name
     @IBOutlet weak var tagTextField: UITextField!  // The text field that takes in the User's Input for the Routine Tag
     @IBOutlet weak var submissionButton: UIButton!  // The Submission button of the information that causes the push to RoutineDatabase table
@@ -23,8 +32,57 @@ class RoutineViewControllerAddNew: UIViewController {
 
         // Do any additional setup after loading the view.
         // Get Text:  nameTextField.text
-        
     }
+    
+    // Runs before a view is displayed
+    override func viewWillAppear(_ animated: Bool)
+    {
+        super.viewWillAppear(animated)
+        
+        // Set Up Collectionview
+        collectionView = UICollectionView(frame: CGRect(x: 0, y: 0, width: 400, height: 200), collectionViewLayout: UICollectionViewLayout())
+        collectionView?.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(collectionView!)
+        //collectionView?.center = self.view.center
+        collectionView?.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        collectionView?.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 160.0).isActive = true
+        collectionView?.widthAnchor.constraint(equalToConstant: 400).isActive = true
+        collectionView?.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        collectionView?.backgroundColor = UIColor.blue
+        
+        // CollectionViewFlowLayout Settings
+        let collectionViewFlowLayout = UICollectionViewFlowLayout()
+        collectionView?.setCollectionViewLayout(collectionViewFlowLayout, animated: true)
+        collectionViewFlowLayout.scrollDirection = .horizontal
+        collectionViewFlowLayout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        collectionViewFlowLayout.itemSize = CGSize.init(width: 150, height: 150)
+        collectionViewFlowLayout.minimumInteritemSpacing = 0
+        collectionViewFlowLayout.minimumLineSpacing = cellSpacing + 10
+        
+        // Collection View Settings
+        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView?.delegate = self
+        collectionView?.dataSource = self
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imageArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        
+        // Cell Setup
+        cell.backgroundColor = .black
+        
+        let cellImageView = UIImageView(frame: CGRect(x:0, y: 0, width: cellWidth, height: cellHeight))
+        cellImageView.image = imageArray[indexPath.row]
+        cell.contentView.addSubview(cellImageView)
+        
+        return cell
+    }
+    
+    // Submit the routine that the user selects
     @IBAction func submitRoutine(_ sender: Any)
     {
         if nameTextField.text == ""
